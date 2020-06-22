6ca8d6365ad96be7407fdd9c490fd2385ad273e2
beesarecute123 <66000745+beesarecute123@users.noreply.github.com>
2020-06-22 11:14:06 +0200

    onepackinother
    added the bi folder into the pack folder

---
+++ newpack/product_bundle_pack/bi_website_product_bundle/__manifest__.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Website/Shop Product Bundle in Odoo",
+    "summary": "App website product pack website product kit shop product bundle shop product pack website product combo shop bundle product bundle product pack kit all in one product bundle pack website product pack ecommerce product bundle pack webshop product pack",
+    "category": "eCommerce",
+    "version": "13.0.0.2",
+    "author": "BrowseInfo",
+    "description": """
+    BrowseInfo developed a new odoo/OpenERP module apps.    
+    Purpose :- 
+    
+Product bundling is offering several products for sale as one combined product. It is a common feature in many imperfectly competitive product markets where price plays important roles, using these module you can act set competitive price for same or different products and variants to increase your sales graph.
+        -Point Of sale Product Bundle
+
+    BrowseInfo developed a new odoo/OpenERP module apps.    
+    Purpose :- 
+    
+    odoo create Product Bundle Product Pack Bundle Pack of Product Combined Product pack odoo
+    odoo Product Pack Custom Combo Product Bundle Product Customized product Group product odoo
+    odoo Custom product bundle Custom Product Pack odoo combo product pack combo product combo bundle pack combo bundle product pack 
+    odoo combo product pack multiple product pack group product pack choice odoo
+    odoo product Pack Price Product Bundle pack price product Bundle Discount product Bundle Offer bundle price
+
+Product bundling is offering several odoo products for sale as one combined product. It is a common feature in many imperfectly competitive product markets where price plays important roles, using these module you can act set competitive price for same or different products and variants to increase your sales graph.
+        odoo Point Of sale Product Bundle POS product bundle ecom webshop Product Bundle ecom
+        odoo ecom webshop Product pack ecom bundle product webshop bundle product store bundle pack
+        odoo store product pack store bundle product Point of sale Pack POS product pack Point of sale product pack
+        odoo Custom pack on POS Combined product on POS Product Pack
+        odoo Custom Combo Product Bundle Product Customized product Group product Custom product bundle. 
+        odoo Custom Product Pack eCommerce product Pack Price Bundle price Bundle Discount website Bundle Offer.
+    This module is use to 
+    odoo website create Product Bundle odoo website Product Pack odoo website Bundle Pack of Product odoo
+    odoo Combined Product pack odoo website Product Pack Custom Combo Product odoo website Bundle Product. 
+    odoo website Customized product Group product Custom product bundle Custom Product Pack odoo website Pack Price
+    odoo website Bundle price Bundle Discount Bundle Offer odoo shop
+    odoo Website Product Bundle Pack Website Product Pack eCommerce Product bundle
+    odoo  All Product bundle feature All IN one product pack
+
+
+    odoo create Product kit Product Pack kit Pack of Product Combined Product kit odoo
+    odoo Product kit Custom Combo Product kit Product Customized product kit product odoo
+    odoo Custom product kit Custom Product kit pack product bundle kit product kit bundle odoo 
+    odoo combo product pack kit combo product combo kit bundle pack combo kit product pack kit 
+    odoo combo product kit multiple product kit group product kit choice odoo
+    odoo product kit Price Product kit pack price product kit Discount product kit Bundle Offer bundle price
+
+
+kit Product bundling is offering several odoo products for sale as one kit product. It is a common feature in many imperfectly website competitive product kit markets module
+        odoo Point Of sale Product kit POS product kit ecom webshop Product kit ecom kit
+        odoo ecom webshop Product kit ecom bundle product webshop bundle kit product store kit bundle pack
+        odoo store product kit store bundle product kit Point of sale product kit POS product kit Point of sale product kit pack
+        odoo Custom product kit on POS Combined product kit on POS Product kit Pack
+        odoo Custom Combo Product Bundle kit Product Customized product Group kit product Custom product bundle kit 
+        odoo Custom Product kit Pack eCommerce product kit Pack Price Bundle kit price Bundle kit Discount website product kit Offer.
+    This module is use to 
+    odoo website create Product kit odoo website Product Pack odoo website kit Pack of Product odoo
+    odoo Combined Product kit odoo website Product kit Custom Combo Product odoo website kit Product. 
+    odoo website Customized product kit product Custom product kit Custom Product kit Pack odoo website Pack kit Price
+    odoo website kit price kit Discount kit Offer odoo shop
+    odoo Website Product Bundle kit Pack Website Product Pack kit eCommerce Product kit bundle
+    odoo All Product bundle kit feature All IN one product kit pack
+    odoo website product combo website product pack webstore product combo pack
+    odoo website products combo webshop product combo website product kit 
+        -POS product bundle , ecom webshop Product Bundle ecom webshop Product pack , ecom bundle product , webshop  bundle product , store bundle pack , store product pack , store bundle product 
+        -Point of sale Pack
+        -POS product pack
+        -Point of sale product pack
+        -Custom pack on POS
+        -Combined product on POS
+        -Product Pack, Custom Combo Product, Bundle Product. Customized product, Group product.Custom product bundle. Custom Product Pack.
+         -Pack Price, Bundle price, Bundle Discount, Bundle Offer.
+	This module is use to create Product Bundle,Product Pack, Bundle Pack of Product, Combined Product pack.
+    -Product Pack, Custom Combo Product, Bundle Product. Customized product, Group product.Custom product bundle. Custom Product Pack.
+    -Pack Price, Bundle price, Bundle Discount, Bundle Offer.
+    -Website Product Bundle Pack, Website Product Pack, eCommerce Product bundle. All Product bundle feature, All IN one product pack 
+    """,
+    "depends": ['product',"product_bundle_pack","website_sale"],
+    "data": [
+        "views/template.xml",
+
+    ],
+    "price": 20,
+    "currency": "EUR",
+    "application": True,
+    "installable": True,
+    "live_test_url":"https://www.youtube.com/watch?v=XMZd_OwyenM",
+    "images":['static/description/Banner.png'],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
