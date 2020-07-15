a36df5a00b07b534f000dfdf08fac671cae8aeb8
Pascal Brunner <pp@vatorex.ch>
2020-07-15 14:32:28 +0200

    Initial Commit

---
+++ vatorex_custom_css_js/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Vatorex: Custom CSS & JS',
+    'description': 'Add custom CSS & Javascript rules to this module for frontend',
+    'category': 'Hidden',
+    'summary': 'Vatorex: Custom CSS & JS',
+    'author': 'Pascal Brunner',
+    'version': '0.1',
+    'depends': ['website', 'website_theme_install', 'website_sale', ],
+    'data': [
+
+        #data
+        'views/assets.xml',
+
+        #pages
+    ],
+
+    "auto_install": True,
+    'installable': True,
+}
