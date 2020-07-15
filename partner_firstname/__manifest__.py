9ffe2b8c520d316edd05aca13bd97a452f280661
Pascal Brunner <pp@vatorex.ch>
2020-07-15 16:57:03 +0200

    [EDIT] manifest name

--- partner_firstname/__manifest__.py
+++ partner_firstname/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Partner first name and last name",
+    "name": "Vatorex: Partner first name and last name",
     "summary": "Split first name and last name for non company partners",
     "version": "13.0.1.0.1",
     "author": "Camptocamp, "
