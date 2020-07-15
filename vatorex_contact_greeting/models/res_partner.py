81a3b0fb864030fd5e6c03bfd32378328a67da94
Pascal Brunner <pp@vatorex.ch>
2020-07-15 17:18:34 +0200

    [EDIT] Gender & functionality

--- vatorex_contact_greeting/models/res_partner.py
+++ vatorex_contact_greeting/models/res_partner.py
@@ -6,13 +6,14 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     greeting_msg = fields.Text(compute='_compute_greeting_msg',store=True, translate=True)
+    gender = fields.Selection([('female', 'F'), ('male', 'M'), ('unknown', 'Unknown')], required=True, default='unknown')
     type_greeting_msg = fields.Selection([('informal', 'Informal'), ('formal', 'Formal')], required=True, default='formal')
 
-    @api.depends('name','type_greeting_msg')
+    @api.depends('firstname', 'lastname','type_greeting_msg', 'gender')
     def _compute_greeting_msg(self):
         for record in self:
             if record.type_greeting_msg == 'informal':
-                record.greeting_msg = record.type_greeting_msg + " " + record.name
+                record.greeting_msg = record.type_greeting_msg + " " + record.name + record.gender
 
             if record.type_greeting_msg == 'formal':
-                record.greeting_msg = record.type_greeting_msg + " " + record.name
+                record.greeting_msg = record.type_greeting_msg + " " + record.name + record.gender
