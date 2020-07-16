263eb34a4e7840af1360d2e8bb93e40a8abf4b21
Pascal Brunner <pp@vatorex.ch>
2020-07-16 10:44:32 +0200

    [ADD] class

--- vatorex_blog_select_lang/models/blog_post.py
+++ vatorex_blog_select_lang/models/blog_post.py
@@ -5,3 +5,7 @@ from odoo import api, fields, models
 class Post(models.Model):
     _inherit = "blog.post"
     language_available_ids = fields.Many2many('res.lang',string='Article languages')
+
+class HTML(models.Model):
+    _inherit = "website.page"
+    language_available_ids = fields.Many2many('res.lang',string='Website languages')
