c5e381c168a3a708faeb4adcc15dc7947f1e8ac3
Pascal Brunner <pp@vatorex.ch>
2020-07-17 09:21:04 +0200

    [ADD] translation to class

--- vatorex_contact_greeting/models/res_partner.py
+++ vatorex_contact_greeting/models/res_partner.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 
-# example_to_translate_with_pot_and_po = _("translate me")
-
-# default greeting messages (english)
-greeting_text_informal = "Hello"
-greeting_text_formal_female = 'Dear Mrs'
-greeting_text_formal_male = 'Dear Mr'
-greeting_text_formal_unknown = 'Dear'
-
-# German
-if res.partner.lang == 'de_DE':
-    greeting_text_informal = "Hallo"
-    greeting_text_formal_female = 'Sehr geehrte Frau'
-    greeting_text_formal_male = 'Sehr geehrter Herr'
-    greeting_text_formal_unknown = 'Sehr geehrte/r'
-
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    # example_to_translate_with_pot_and_po = _("translate me")
+
+    # English (default greeting messages)
+    greeting_text_informal = "Hello"
+    greeting_text_formal_female = 'Dear Mrs'
+    greeting_text_formal_male = 'Dear Mr'
+    greeting_text_formal_unknown = 'Dear'
+
+    # German
+    if res.partner.lang == 'de_DE':
+        greeting_text_informal = "Hallo"
+        greeting_text_formal_female = 'Sehr geehrte Frau'
+        greeting_text_formal_male = 'Sehr geehrter Herr'
+        greeting_text_formal_unknown = 'Sehr geehrte/r'
 
     greeting_msg = fields.Text(compute='_compute_greeting_msg',store=True, translate=True)
     gender = fields.Selection([('female', 'Female'), ('male', 'Male'), ('unknown', 'Unknown')], required=True, default='unknown')
