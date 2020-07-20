0659fe185c2e51fad474bb7c74d398b8d4784a6e
Pascal Brunner <pp@vatorex.ch>
2020-07-20 12:13:53 +0200

    [ADD] condition

--- vatorex_custom_css_js/static/src/js/main.js
+++ vatorex_custom_css_js/static/src/js/main.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     // Display Alt-Tag in images in Blog
     var blog = $( "body" ).not(".editor_enable").find($("#o_wblog_post_content"));
     blog.find('img').each(function() {
-        var alt = this.alt;
-        $(this).after('<div class="figure-caption">'+ alt + '</div>');
+        var data_caption = this.attr("data-caption")
+        if(!data_caption) {
+            var alt = this.alt;
+            this.data("caption", alt);
+            $(this).after('<div class="figure-caption">'+ alt + '</div>');
+        }
     });
 
     // Additional jQuery here
