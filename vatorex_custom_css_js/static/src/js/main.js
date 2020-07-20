935ba5a8f5706ffbfbe155fe0b20b30d51ceae0e
Pascal Brunner <pp@vatorex.ch>
2020-07-20 11:58:10 +0200

    [Hide] alt tag caption in edit mode

--- vatorex_custom_css_js/static/src/js/main.js
+++ vatorex_custom_css_js/static/src/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     // Display Alt-Tag in images in Blog
-    $("#o_wblog_post_content").find('img').each(function() {
-         var alt = this.alt;
+    var blog = $( "body" ).not(".editor_enable").find($("#o_wblog_post_content"));
+    blog.find('img').each(function() {
+        var alt = this.alt;
         $(this).after('<div class="figure-caption">'+ alt + '</div>');
     });
 
