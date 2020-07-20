165724ee063bcbfd9b591a6a3e47cd13a2843548
Pascal Brunner <pp@vatorex.ch>
2020-07-20 12:37:39 +0200

    initial commit

---
+++ vatorex_blog_figure/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Vatorex: blog figure',
+    'category': 'Hidden',
+    'sequence': 7,
+    'summary': 'Vatorex: blog figure',
+    'version': '0.1',
+    'description': '',
+    'depends': [
+        'website',
+        'website_mail',
+        'website_form',
+        'website_crm',
+    ],
+    'images': [
+        'static/description/website_theme_screenshot.jpg',
+    ],
+
+    'data': [
+
+        # data
+
+        # assets
+
+        # layout
+
+        #backend
+
+        # pages
+        'views/backend/building_block.xml',
+
+        # generic views
+
+    ],
+
+}
