c41e9d63c96d480db6f806eb1b99d8aa7ac86a10
Pascal Brunner <pp@vatorex.ch>
2020-09-14 16:24:54 +0200

    EDIT js

--- vatorex_custom_css_js/static/src/js/main.js
+++ vatorex_custom_css_js/static/src/js/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-    // Display Alt-Tag in images in Blog (deactivated)
-    var blog = $( "body" ).not(".editor_enable").find($("#o_wblog_post_content"));
-    blog.find('img').each(function() {
-        var data_vatorex_caption = this.data("vatorex_caption")
-        if(!data_caption) {
-            //var alt = this.alt;
-            //this.data("vatorex_caption", alt);
-            //$(this).after('<div class="figure-caption">'+ alt + '</div>');
-        }
-    });
-
     // Additional jQuery here
     console.log('custom js is ready');
 });
