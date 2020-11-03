947a239835ed045d5be093580d24e12e218d65a7
Pascal Brunner <pp@vatorex.ch>
2020-11-03 17:18:56 +0100

    EDIT url translatable

--- vatorex_blog_select_lang/models/blog_post.py
+++ vatorex_blog_select_lang/models/blog_post.py
@@ -6,7 +6,9 @@ class Post(models.Model):
     _inherit = "blog.post"
     language_available_ids = fields.Many2many('res.lang',string='Article languages')
 
-# make page title translatable
+# make page title & url translatable
 class HTML(models.Model):
     _inherit = "website.page"
     name = fields.Char(translate=True)
+    url = fields.Char(translate=True)
+    
