b5c37dc1861c5a68aad155e7a96333824ab60427
Pascal Brunner <pp@vatorex.ch>
2020-11-03 16:42:57 +0100

    ADD comments

--- vatorex_blog_select_lang/models/blog_post.py
+++ vatorex_blog_select_lang/models/blog_post.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
-
+# make blog language selectable
 class Post(models.Model):
     _inherit = "blog.post"
     language_available_ids = fields.Many2many('res.lang',string='Article languages')
 
+# make page title translatable
 class HTML(models.Model):
     _inherit = "website.page"
     name = fields.Char(translate=True)
-    language_available_ids = fields.Many2many('res.lang',string='Website languages')
