d190d0c6bc85c3ee55a4da50a178c7650e274d8f
Pascal Brunner <pp@vatorex.ch>
2020-11-03 13:19:11 +0100

    EDIT descr

--- vatorex_inherit_models/models/website_page.py
+++ vatorex_inherit_models/models/website_page.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 
 class website_page(models.Model):
     _inherit = "website.page"
-    name = fields.Char(translate=True)
+    name = fields.Char(string='Description', translate=True)
