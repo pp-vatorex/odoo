181821f33d7655d7801eda0cc5555c4eb14e8d86
Pascal Brunner <pp@vatorex.ch>
2021-03-09 10:59:54 +0100

    uncomment

--- vatorex_blog_select_lang/controllers/blog.py
+++ vatorex_blog_select_lang/controllers/blog.py
@@ -24,5 +24,5 @@ class WebsiteBlog(WebsiteBlog):
         if posts:
             current_lang = request.env['res.lang'].search([('code','=',request.context['lang'])])
             s = posts.filtered(lambda r: current_lang in r.language_available_ids )
-        # result.qcontext.update({'posts':s})
+            result.qcontext.update({'posts':s})
         return result
