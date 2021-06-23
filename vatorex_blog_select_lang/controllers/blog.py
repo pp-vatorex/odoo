4c27977a30fcb600070e05e5629e5c58b27193f7
Pascal Brunner <pp@vatorex.ch>
2021-06-23 10:51:12 +0200

    rm blog

--- vatorex_blog_select_lang/controllers/blog.py
+++ vatorex_blog_select_lang/controllers/blog.py
@@ -7,21 +7,21 @@ from odoo.addons.website.controllers.main import QueryURL
 
 class WebsiteBlog(WebsiteBlog):
 
-    @http.route([
-        '/blog',
-        '/blog/page/<int:page>',
-        '/blog/tag/<string:tag>/page/<int:page>',
-        '''/blog/<model("blog.blog", "[('website_id', 'in', (False, current_website_id))]"):blog>''',
-        '''/blog/<model("blog.blog"):blog>/page/<int:page>''',
-        '''/blog/<model("blog.blog"):blog>/tag/<string:tag>''',
-        '''/blog/<model("blog.blog"):blog>/tag/<string:tag>/page/<int:page>''',
-    ], type='http', auth="public", website=True)
+    # @http.route([
+    #     '/blog',
+    #     '/blog/page/<int:page>',
+    #     '/blog/tag/<string:tag>/page/<int:page>',
+    #     '''/blog/<model("blog.blog", "[('website_id', 'in', (False, current_website_id))]"):blog>''',
+    #     '''/blog/<model("blog.blog"):blog>/page/<int:page>''',
+    #     '''/blog/<model("blog.blog"):blog>/tag/<string:tag>''',
+    #     '''/blog/<model("blog.blog"):blog>/tag/<string:tag>/page/<int:page>''',
+    # ], type='http', auth="public", website=True)
 
-    def blog(self, blog=None, tag=None, page=1, **opt):
-        result = super(WebsiteBlog,self).blog(blog, tag, page, **opt)
-        posts = result.qcontext['posts']
-        if posts:
-            current_lang = request.env['res.lang'].search([('code','=',request.context['lang'])])
-            s = posts.filtered(lambda r: current_lang in r.language_available_ids )
-        result.qcontext.update({'posts':s})
-        return result
+    # def blog(self, blog=None, tag=None, page=1, **opt):
+    #     result = super(WebsiteBlog,self).blog(blog, tag, page, **opt)
+    #     posts = result.qcontext['posts']
+    #     if posts:
+    #         current_lang = request.env['res.lang'].search([('code','=',request.context['lang'])])
+    #         s = posts.filtered(lambda r: current_lang in r.language_available_ids )
+    #     result.qcontext.update({'posts':s})
+    #     return result
